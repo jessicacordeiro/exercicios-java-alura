a9b0f0f6fa52028d399d2052261a5762f7681e6d
jessicacordeiro <jessica.cordeiro121@gmail.com>
2021-06-01 14:57:06 -0300

    Exercícios do dia 01/06/21

---
+++ sintaxe-variaveis-e-fluxo/src/Fatorial.java
@@ -0,0 +1,11 @@
+
+public class Fatorial {
+	public static void main(String[] args) {
+		int fatorial = 1; 
+		
+		for(int f = 1; f <= 11; f++) {
+			fatorial *= f;
+			System.out.println("Fatorial de " + f + " = " + fatorial);
+		}
+	}
+}
