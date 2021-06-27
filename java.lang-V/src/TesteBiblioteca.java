ea0abdaaf8a270441693081ed27765a804df9177
jessicacordeiro <jessica.cordeiro121@gmail.com>
2021-06-26 22:35:11 -0300

    Exercícios de Java e java.lang: Programe com a classe Object e String.

---
+++ java.lang-V/src/TesteBiblioteca.java
@@ -0,0 +1,11 @@
+import br.com.bytebank.banco.modelo.Conta;
+import br.com.bytebank.banco.modelo.ContaCorrente;
+
+public class TesteBiblioteca {
+	
+	public static void main(String[] args) {
+		
+		Conta c = new ContaCorrente(1256, "15569-9");
+	}
+
+}
---
+++ java.lang-V/src/TesteString.java
@@ -0,0 +1,11 @@
+
+public class TesteString {
+	
+	public static void main(String[] args) {
+		
+		String nome = "Mario";
+		nome.replace('o', 'a');
+		System.out.println(nome);
+	}
+
+}
